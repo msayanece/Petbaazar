d22cb1c11b1c8e4befd9984c194f5492bba26eeb
Sayan <msayanece@gmail.com>
2017-01-10 20:22:32 +0530

    add doBackgroundWorker all code

--- app/src/main/java/com/brandtechnosolutions/petbaazar/DatabaseBackgroundWorker.java
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/DatabaseBackgroundWorker.java
@@ -3,6 +3,18 @@ package com.brandtechnosolutions.petbaazar;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 /**
  * Created by user on 1/10/2017.
  */
@@ -26,8 +38,40 @@ public class DatabaseBackgroundWorker extends AsyncTask<String, Void, String> {
         if (strings[5]!= null){
             email = strings[5];
         }
+        String login_url = "http://192.168.1.6/login.php";
+        if (type.equals("login")) {
+            try {
 
-
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(firstName, "UTF-8") + "&"
+                        + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(lastName, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
     }
 
