315a375654c32fe924d7661cb0e9cce7513de711
Sayan <msayanece@gmail.com>
2017-01-12 13:38:10 +0530

    create BuyerFragment and create SellerFragment

---
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/BuyerFragment.java
@@ -0,0 +1,21 @@
+package com.brandtechnosolutions.petbaazar;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BuyerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_buyer, container, false);
+    }
+}
