fa1d2cb0114317d34f5a1b97cc21bb5361936c2e
Sayan <msayanece@gmail.com>
2017-01-17 17:40:01 +0530

    update takeadinfoactivity, set on click method

--- app/src/main/java/com/brandtechnosolutions/petbaazar/OptionActivity.java
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/OptionActivity.java
@@ -1,9 +1,9 @@
 package com.brandtechnosolutions.petbaazar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -37,16 +37,11 @@ public class OptionActivity extends AppCompatActivity {
         post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onSell(view);
+                Intent intent = new Intent(OptionActivity.this, TakeAdInfoActivity.class);
+                startActivity(intent);
                 Snackbar.make(view, "Want to capture?", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    void onSell(View view) {
-        FragmentManager manager = getSupportFragmentManager();
-        SellerOptionFragment dialogFragment = new SellerOptionFragment();
-        dialogFragment.show(manager, "Alert");
     }
 }
--- app/src/main/java/com/brandtechnosolutions/petbaazar/TakeAdInfoActivity.java
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/TakeAdInfoActivity.java
@@ -1,7 +1,9 @@
 package com.brandtechnosolutions.petbaazar;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TakeAdInfoActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class TakeAdInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_ad_info);
+    }
+
+
+    void onOk(View view) {
+        FragmentManager manager = getSupportFragmentManager();
+        SellerOptionFragment dialogFragment = new SellerOptionFragment();
+        dialogFragment.show(manager, "Alert");
     }
 }
