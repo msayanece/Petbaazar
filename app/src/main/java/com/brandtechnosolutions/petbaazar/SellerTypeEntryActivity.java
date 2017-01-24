dc13ca35b5f0932067d8c35a9bfd7b4bc9b9417c
Sayan <msayanece@gmail.com>
2017-01-24 15:16:31 +0530

    seller type entry activity java code update 2, edit text auto complete for location, create CountryDetails class for retrieving all the country name

--- app/src/main/java/com/brandtechnosolutions/petbaazar/SellerTypeEntryActivity.java
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/SellerTypeEntryActivity.java
@@ -1,15 +1,16 @@
 package com.brandtechnosolutions.petbaazar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class SellerTypeEntryActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private static final String[] COUNTRIES = new CountyDetails().getCountry();
     Spinner spinner1, spinner2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,14 @@ public class SellerTypeEntryActivity extends AppCompatActivity implements Adapte
                 this, R.array.business_type, android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(spinnerAdapter);
         spinner2.setOnItemSelectedListener(this);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.location_edit_text_auto_textview_id);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        textView.setAdapter(adapter);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show();
     }
 
     @Override
