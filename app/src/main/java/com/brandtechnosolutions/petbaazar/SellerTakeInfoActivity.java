29eecc1efd863b8cf2818537235a3338aef4b1b4
Sayan <msayanece@gmail.com>
2017-01-24 12:44:00 +0530

    add seller take info activity and seller type entry activity

---
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/SellerTakeInfoActivity.java
@@ -0,0 +1,13 @@
+package com.brandtechnosolutions.petbaazar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SellerTakeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_take_info);
+    }
+}
---
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/SellerTypeEntryActivity.java
@@ -0,0 +1,13 @@
+package com.brandtechnosolutions.petbaazar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SellerTypeEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_type_entry);
+    }
+}
