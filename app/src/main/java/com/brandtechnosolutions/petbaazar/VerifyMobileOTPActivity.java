33205292ec3b41db4b34089fd7fd8ecfcce5840e
Sayan <msayanece@gmail.com>
2017-01-25 11:02:21 +0530

    Create verify mobile OTP activity

---
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/VerifyMobileOTPActivity.java
@@ -0,0 +1,13 @@
+package com.brandtechnosolutions.petbaazar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyMobileOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_mobile_otp);
+    }
+}
