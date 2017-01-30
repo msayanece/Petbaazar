5bc82bbe08878dbc64cb864c42ef5bcdda48aee7
msayanece <msayanece@gmail.com>
2017-01-30 20:25:54 +0530

    update seller take info activity UI almost complete

--- app/src/main/java/com/brandtechnosolutions/petbaazar/SellProductFirstActivity.java
+++ app/src/main/java/com/brandtechnosolutions/petbaazar/SellProductFirstActivity.java
@@ -4,19 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class SellProductFirstActivity extends AppCompatActivity {
 
+    private EditText emailEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_product_first);
+        emailEditText = (EditText) findViewById(R.id.seller_email_edit_text_id);
     }
 
     public void sellerLogin(View view) {
 //        Snackbar.make(view, "This part is under development!", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show();
+        String tempEmail = emailEditText.getText().toString();
         Intent intent = new Intent(SellProductFirstActivity.this, PetbaazarSellerMainActivity.class);
+        intent.putExtra("email", tempEmail);
         startActivity(intent);
     }
 
